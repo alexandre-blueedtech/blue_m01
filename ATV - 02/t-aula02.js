618f857c47754cdb73bde0bfeab921e441f128ea
lucasvm1 <lucas.vianam@outlook.com>
2022-01-19 18:33:50 -0300

    main

--- ATV - 02/t-aula02.js
+++ ATV - 02/t-aula02.js
@@ -69,5 +69,55 @@ console.table(alunos);
 
 */
 
+/* EXERCICO 5
+
+console.log();
+let cliente = prompt(`Qual seu nome? `);
+console.log();
+
+console.log();
+var escolha = prompt(`Qual desses produtos você quer? `);
+console.log();
+
+console.log(`Ótimo ${cliente}! ${escolha} é uma ótima escolha!`);
+
+*/
 
 
+var monstro = +prompt(`Escolha a vida do monstro de 10 a 50: `);
+
+while(monstro < 10 || monstro > 50) {
+    console.log();
+    console.log(`O número ${monstro} não é válido! Por favor escolha um número entre 10 e 50!`)
+    console.log();
+    var monstro = +prompt(`Escolha a vida do monstro de 10 a 50: `);
+}
+
+var heroi = +prompt(`Escolha a força de ataque do seu héroi de 5 a 10: `);
+
+while(heroi < 5 || heroi > 10) {
+    console.log();
+    console.log(`O número ${heroi} não é válido! Por favor escolha um número entre 5 e 10!`)
+    console.log();
+    var heroi = +prompt(`Escolha a força de ataque do seu héroi de 5 a 10: `);
+}
+
+let turnos = (monstro / heroi);
+let rturnos = Math.ceil(turnos);
+
+console.clear();
+
+console.log();
+console.log('---------------------------------------------------')
+console.log();
+// -----------------------------------
+console.log(`Vida do Monstro: ${monstro}` );
+console.log();
+console.log(`Poder de ataque do Héroi: ${heroi} `);
+console.log();
+console.log();
+console.log(`Você derrotará o monstro em ${rturnos} turnos`);
+// -----------------------------------
+console.log();
+console.log('---------------------------------------------------')
+console.log();

24826660e07ee12296eebd7d8d7ff1ee7a0c6c53
lucasvm1 <lucas.vianam@outlook.com>
2022-01-19 22:31:14 -0300

    main

--- ATV - 03/ex02.js
+++ ATV - 03/ex02.js
@@ -16,7 +16,7 @@ const prompt = require('prompt-sync')();
 
  if(media >= 7){
      console.log('Aprovado');
- } else if(media < 7 && media >= 5 ){
+ } else if(media >= 5 ){
      console.log('Recuperação');
  } else {
      console.log('Reprovado')
--- ATV - 03/ex04.js
+++ ATV - 03/ex04.js
@@ -4,4 +4,13 @@ const prompt = require('prompt-sync')();
 
  // ---------------------------------------- //
 
- 
+ let num1 = +prompt('Digite um número: ');
+ let num2 = +prompt('Digite outro número: ')
+ let div1 = num1 % num2;
+ let div2 = num2 % num1;
+
+ if(div1 != 0 && div2 != 0){
+     console.log(`Os números ${num1} e ${num2} não são múltiplos!`)
+ } else {
+     console.log(`Os números ${num1} e ${num2} são múltiplos!`)
+ }
